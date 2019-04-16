0e6a731e96b55b0a2e5485461c1c2dd14bfb3262
Pauline Rudge <paulinerudge@me.com>
2019-04-16 23:18:55 +0100

    added audio

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -6,9 +6,16 @@ const SongItem = (props) => {
     <div className = "song-item-div">
       <img className= "image" src = {props.tune['im:image'][2]['label']} />
       <p className = "position">{props.children}. {props.tune['im:name'].label}</p>
-      <p className = "artist">{props.tune['im:artist'].label}</p>
+    <p className = "artist">{props.tune['im:artist'].label}</p>
+    <p><audio
+        controls
+        src={props.tune['link'][1]['attributes']['href']}>
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio></p>
     </div>
   )
+
 }
 
 export default SongItem;
