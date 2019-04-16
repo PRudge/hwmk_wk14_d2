2b372ebf06e56e52a9528f8e1c3aaa0044088f86
Pauline Rudge <paulinerudge@me.com>
2019-04-16 23:30:18 +0100

    centered sound bar in boxes

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -7,7 +7,7 @@ const SongItem = (props) => {
       <img className= "image" src = {props.tune['im:image'][2]['label']} />
       <p className = "position">{props.children}. {props.tune['im:name'].label}</p>
     <p className = "artist">{props.tune['im:artist'].label}</p>
-    <p><audio
+    <p><audio className = "audio"
         controls
         src={props.tune['link'][1]['attributes']['href']}>
             Your browser does not support the
